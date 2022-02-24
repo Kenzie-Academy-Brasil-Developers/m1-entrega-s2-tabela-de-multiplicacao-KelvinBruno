f955f9233222f6862920d97d595ff57b40a7d21f
Kelvin Lopes <kelvinbrunolopes@gmail.com>
2022-02-24 12:52:09 -0300

    finalizado

---
+++ tabelaJS.js
@@ -0,0 +1,17 @@
+function tabela(a){
+    let ar = []
+    let multiplicacao = 0;
+for(i=0;i<=a;i++){
+    const num = []
+    for(j = 0; j<=a;j++){
+     ;
+    num.push(multiplicacao)
+        
+    }
+    ar.push(num)
+
+}
+console.table(ar)
+}
+
+
